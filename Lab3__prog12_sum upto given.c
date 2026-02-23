f0d9702be10fb828d15b7e13c21ec8b137548f39
tanmay911-web <tanmaychaudhary11111@gmail.com>
2026-02-23 21:54:47 +0530

    Create Lab3__prog12_sum upto given.c

---
+++ Lab3__prog12_sum upto given.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+int n, sum=0;
+printf("enter the number:");
+scanf("%d",&n);
+for (int i = 0; i <=n; i++)
+{
+    sum=sum+i;
+}
+{
+    printf("the sum of upto given no. is:%d",sum);
+   
+}
+ return 0;
+}
